695e2fac9e3b7af68098b3aa35ab6bc7293cf2b6
Joanh.Fu <ferock@gmail.com>
2020-01-20 17:26:11 +0800

    format code

--- main.go
+++ main.go
@@ -7,19 +7,19 @@ import (
 	"net/http"
 )
 
-func main()  {
+func main() {
 
 	config := service.GetEnvConfig()
 	//读取env配置 print test
-	for key,value:= range config{
-		fmt.Printf("%s=>%s\n",key,value)
+	for key, value := range config {
+		fmt.Printf("%s=>%s\n", key, value)
 	}
 
 	var routeHandle = new(controller.RouteHandle)
 	//托管回调 必须在服务开启之前
 	RegistRoute(routeHandle)
 	//监听服务
-	err := http.ListenAndServe("0.0.0.0:" + config["server_port"],routeHandle)
+	err := http.ListenAndServe("0.0.0.0:"+config["server_port"], routeHandle)
 	if err != nil {
 		fmt.Println("create http server error" + err.Error())
 	}
@@ -27,6 +27,6 @@ func main()  {
 }
 
 // 注册各个控制器路由
-func RegistRoute(handle *controller.RouteHandle)  {
+func RegistRoute(handle *controller.RouteHandle) {
 	new(controller.UserController).Router(handle)
 }
